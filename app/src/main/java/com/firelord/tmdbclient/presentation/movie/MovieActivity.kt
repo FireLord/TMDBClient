00362d8d79435f9b98c825d3723a2029107592a5
FireLord <firelord.xda@gmail.com>
2023-03-22 05:37:54 +0530

    movieActivity: Add update menu item
    Signed-off-by: FireLord <firelord.xda@gmail.com>

--- app/src/main/java/com/firelord/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/firelord/tmdbclient/presentation/movie/MovieActivity.kt
@@ -1,6 +1,9 @@
 package com.firelord.tmdbclient.presentation.movie
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -48,6 +51,36 @@ class MovieActivity : AppCompatActivity() {
             }else{
                 binding.movieProgressBar.visibility = View.VISIBLE
                 Toast.makeText(applicationContext,"No data available",Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return  when (item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateMovies(){
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
             }
         })
     }
